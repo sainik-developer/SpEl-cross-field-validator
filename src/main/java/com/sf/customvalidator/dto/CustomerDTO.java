0b8e0413ecfe415cfa8dd4d11d263158497c5521
sainik <sainik.developer.2017@gmail.com>
2021-08-04 14:05:00 +0530

    unit cases are fixed

--- src/main/java/com/sf/customvalidator/dto/CustomerDTO.java
+++ src/main/java/com/sf/customvalidator/dto/CustomerDTO.java
@@ -1,35 +1,42 @@
 package com.sf.customvalidator.dto;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sf.customvalidator.constant.CustomerType;
 import com.sf.customvalidator.validator.CrossFieldValidator;
 import com.sf.customvalidator.validator.SpElCrossFieldCondition;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
-@CrossFieldValidator(groups = {PostMapping.class, PutMapping.class}, conditions = {
-        @SpElCrossFieldCondition(IF = "customerType == T(com.sf.customvalidator.constant.CustomerType).ORGANIZATION", THEN = "surname==null"),
-        @SpElCrossFieldCondition(IF = "customerType == T(com.sf.customvalidator.constant.CustomerType).PERSON", THEN = "surname!=null AND !surname.isEmpty()")
+@Builder
+@CrossFieldValidator(groups = {PostMapping.class}, conditions = {
+        @SpElCrossFieldCondition(IF = "customerType == T(com.sf.customvalidator.constant.CustomerType).ORGANIZATION",
+                THEN = "surname==null"),
+        @SpElCrossFieldCondition(IF = "customerType == T(com.sf.customvalidator.constant.CustomerType).ORGANIZATION",
+                THEN = "dob == null AND doi != null"),
+        @SpElCrossFieldCondition(IF = "customerType == T(com.sf.customvalidator.constant.CustomerType).ORGANIZATION",
+                THEN = "addresses!=null AND addresses.size() == 1"),
+        @SpElCrossFieldCondition(IF = "customerType == T(com.sf.customvalidator.constant.CustomerType).PERSON",
+                THEN = "surname!=null AND !surname.isEmpty()"),
+        @SpElCrossFieldCondition(IF = "customerType == T(com.sf.customvalidator.constant.CustomerType).PERSON",
+                THEN = "dob != null AND doi == null"),
+        @SpElCrossFieldCondition(IF = "customerType == T(com.sf.customvalidator.constant.CustomerType).PERSON",
+                THEN = "addresses!=null AND addresses.size() >= 1 AND addresses.size() <= 2")
 })
 public class CustomerDTO {
     @NotEmpty(groups = PostMapping.class)
     private String name;
-    @NotEmpty(groups = PostMapping.class)
     private String surname;
-    @NotEmpty(groups = PostMapping.class)
     private LocalDate dob;
+    private LocalDate doi;
     private List<@Valid AddressDTO> addresses;
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private String id;
-    @NotEmpty(groups = PostMapping.class)
+    @NotNull(groups = PostMapping.class)
     private CustomerType customerType;
 }
-
--- src/test/java/com/sf/customvalidator/SpringValidatorTest.java
+++ src/test/java/com/sf/customvalidator/SpringValidatorTest.java
@@ -1,5 +1,8 @@
 package com.sf.customvalidator;
 
+import com.sf.customvalidator.constant.Country;
+import com.sf.customvalidator.constant.CustomerType;
+import com.sf.customvalidator.dto.AddressDTO;
 import com.sf.customvalidator.dto.CustomerDTO;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Set;
 
 @SpringBootTest
@@ -24,8 +29,21 @@ public class SpringValidatorTest {
 
     @Test
     public void testSuccess() {
-        final CustomerDTO customerDTO = new CustomerDTO();
-        Set<ConstraintViolation<CustomerDTO>> constraintViolations = validator.validate(customerDTO, PostMapping.class);
+        Set<ConstraintViolation<CustomerDTO>> constraintViolations = validator.validate(CustomerDTO.builder().customerType(CustomerType.ORGANIZATION).
+                name("ABC LLP").doi(LocalDate.of(2020, 01, 01))
+                .addresses(Collections.singletonList(AddressDTO.builder()
+                        .houseNo("6/7").lane("lane").areaCode("22222")
+                        .country(Country.US).state("NY").build())).build(), PostMapping.class);
         Assert.assertEquals(constraintViolations.size(), 0);
+    }
+
+    @Test
+    public void testFailureOnAddressAreaCode() {
+        Set<ConstraintViolation<CustomerDTO>> constraintViolations = validator.validate(CustomerDTO.builder().customerType(CustomerType.ORGANIZATION).
+                name("ABC LLP").doi(LocalDate.of(2020, 01, 01))
+                .addresses(Collections.singletonList(AddressDTO.builder()
+                        .houseNo("6/7").lane("lane").areaCode("222222")
+                        .country(Country.US).state("NY").build())).build(), PostMapping.class);
+        Assert.assertEquals(constraintViolations.size(), 2);
     }
 }
